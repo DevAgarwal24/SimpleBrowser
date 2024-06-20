7ac33e9ab35b59846914b2c5fb3306b13ca4a6fc
Deven Agarwal <dvn.agarwal24@gmail.com>
2024-06-20 19:28:10 +0530

    Add support for &lt; and &gt; entities

--- browser.py
+++ browser.py
@@ -7,11 +7,31 @@ file = f'{os.path.dirname(os.path.realpath(__file__))}/demo_file.txt'
 
 def show( body ):
     in_tag = False
+    entity = ''
+    skip_entity = False
+
     for c in body:
         if c == '<':
             in_tag = True
         elif c == '>':
             in_tag = False
+        elif c == '&':
+            skip_entity = True
+            entity = '&'
+        elif skip_entity:
+            entity += c
+            if entity == '&lt;':
+                print('<', end="")
+                skip_entity = False
+                entity = ''
+            elif entity == '&gt;':
+                print('>', end="")
+                skip_entity = False
+                entity = ''
+            elif entity.endswith(';'):
+                print(entity, end="")
+                skip_entity = False
+                entity = ''
         elif not in_tag:
             print( c, end="" )
 
